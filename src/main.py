f6a15b43445047a9ce9c26fee9de1b08998ea9b2
DESKTOP-PVJ8BUN\d <dhkim1920@gmail.com>
2024-08-29 23:30:40 +0900

    #4 [AM-4] 스케쥴 설정 기능
    - 스케쥴 설정 UI 및 기능 추가

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
+import argparse
 import ctypes
+import json
+import os
 import sys
 import threading
 import time
 import tkinter as tk
-from tkinter import messagebox
-import pyautogui
-import json
-import os
-import argparse
-import keyboard
-import pydirectinput
+from tkinter import messagebox, ttk
 
+import keyboard
+import pyautogui
+import pydirectinput
+import schedule
 
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
@@ -18,6 +19,55 @@ else:
     application_path = os.path.dirname(os.path.abspath(__file__))
 
 settings_file = os.path.join(application_path, "../coordinates.json")
+
+
+def schedule_task(day=None, time_str="00:00:00", weekdays=None, weekends=None):
+    try:
+        if day:
+            schedule.every().day.at(time_str).do(on_click).tag(day)
+        elif weekdays:
+            for day in ["monday", "tuesday", "wednesday", "thursday", "friday"]:
+                schedule.every().day.at(time_str).do(on_click).tag(day)
+        elif weekends:
+            for day in ["saturday", "sunday"]:
+                schedule.every().day.at(time_str).do(on_click).tag(day)
+        elif weekdays:
+            for day in weekdays:
+                getattr(schedule.every(), day).at(time_str).do(on_click)
+        else:
+            schedule.every().week.at(time_str).do(on_click)
+    except schedule.ScheduleValueError as e:
+        messagebox.showwarning(title="스케줄 오류", message="유효한 스케줄을 입력하세요.")
+        raise schedule.ScheduleValueError(e)
+
+
+def run_scheduler():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def add_schedule(time_entry, schedule_type_var, weekday_vars):
+    time_str = time_entry.get()
+    schedule_type = schedule_type_var.get()
+    selected_days = [day for day, var in weekday_vars.items() if var.get()]
+
+    try:
+        if schedule_type == "매주":
+            schedule_task(time_str=time_str)
+        elif schedule_type == "평일":
+            schedule_task(weekdays=True, time_str=time_str)
+        elif schedule_type == "주말":
+            schedule_task(weekends=True, time_str=time_str)
+        elif schedule_type == "특정 요일" and selected_days:
+            schedule_task(weekdays=selected_days, time_str=time_str)
+        else:
+            messagebox.showwarning(title="스케줄 오류", message="유효한 스케줄을 입력하세요.")
+            return
+    except schedule.ScheduleValueError as e:
+        return
+
+    messagebox.showinfo(title="스케줄 추가", message="스케줄이 성공적으로 추가되었습니다.")
 
 
 def save_coordinates():
@@ -126,6 +176,22 @@ def listen_for_hotkey():
         keyboard.add_hotkey('ctrl+s', save_mouse_position_by_hotkey)
 
 
+
+
+
+def select_days(weekday_vars, day):
+    # Update the selected_day variable with the clicked button's day
+    tk.StringVar().set(day)
+
+    # Reset all weekday_vars to False
+    for day_var in weekday_vars.values():
+        day_var.set(False)
+
+    # Set the clicked day to True
+    if day in weekday_vars:
+        weekday_vars[day].set(True)
+
+
 def create_gui():
     global x_entry, y_entry, click_type_var, position_label, root
 
@@ -144,6 +210,49 @@ def create_gui():
     click_type_var = tk.StringVar(value="Click")
     tk.Radiobutton(root, text="클릭", variable=click_type_var, value="Click").grid(row=2, column=1)
     tk.Radiobutton(root, text="더블 클릭", variable=click_type_var, value="DoubleClick").grid(row=2, column=2)
+
+    frame = ttk.Frame(root)
+    frame.grid(row=7, column=1)
+
+    ttk.Label(frame, text="시간 (HH:MM:SS):").grid(row=0, column=0, sticky=tk.W)
+    time_entry = ttk.Entry(frame)
+    time_entry.grid(row=0, column=1)
+
+    # 스케줄 타입 선택
+    # schedule_type_var = tk.StringVar(value="매주")
+    # ttk.Label(frame, text="스케줄 타입:").grid(row=1, column=0, sticky=tk.W)
+    # schedule_type_combo = ttk.Combobox(frame, textvariable=schedule_type_var, state="readonly")
+    # schedule_type_combo['values'] = ["매주", "평일", "주말", "특정 요일"]
+    # schedule_type_combo.grid(row=1, column=1)
+
+    # 특정 요일 선택 (특정 요일을 선택할 때만 활성화)
+    weekday_vars = {
+        "monday": tk.BooleanVar(),
+        "tuesday": tk.BooleanVar(),
+        "wednesday": tk.BooleanVar(),
+        "thursday": tk.BooleanVar(),
+        "friday": tk.BooleanVar(),
+        "saturday": tk.BooleanVar(),
+        "sunday": tk.BooleanVar(),
+    }
+
+    everyday_button = tk.Button(root, text="매일", command=lambda: select_days(weekday_vars, "everyday"))
+    everyday_button.grid(row=8, column=0, columnspan=2)
+    weekday_button = tk.Button(root, text="평일", command=lambda: select_days(weekday_vars, "weekday"))
+    weekday_button.grid(row=8, column=1, columnspan=2)
+    weekend_button = tk.Button(root, text="주말", command=lambda: select_days(weekday_vars, "weekend"))
+    weekend_button.grid(row=8, column=2, columnspan=2)
+
+
+    weekday_frame = ttk.LabelFrame(frame, text="특정 요일 선택")
+    weekday_frame.grid(row=2, column=0, columnspan=2)
+    for i, (day, var) in enumerate(weekday_vars.items()):
+        chk = ttk.Checkbutton(weekday_frame, text=day.capitalize(), variable=var)
+        chk.grid(row=0, column=i, sticky=tk.W)
+
+    add_button = ttk.Button(frame, text="스케줄 설정",
+                            command=lambda: add_schedule(time_entry, weekday_vars))
+    add_button.grid(row=3, column=0, columnspan=2)
 
     position_label = tk.Label(root, text="현재 마우스 위치: ")
     position_label.grid(row=3, column=0, columnspan=2)
@@ -192,6 +301,9 @@ def rerun_as_admin(root):
 
 if __name__ == "__main__":
     root = create_gui()
+
+    scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
+    scheduler_thread.start()
 
     root.bind("<FocusIn>", on_focus)
     root.bind("<FocusOut>", on_focus_lost)
